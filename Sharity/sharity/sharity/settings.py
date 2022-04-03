c7b74143e137bb51cf3c614a5346d745f7b8f372
Osman Selçuk Sarıoğlu <ossarioglu@gmail.com>
2022-04-03 14:21:25 +0300

    Dockerization is done

--- Sharity/sharity/sharity/settings.py
+++ Sharity/sharity/sharity/settings.py
@@ -78,6 +78,7 @@ WSGI_APPLICATION = 'sharity.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -88,7 +89,18 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+"""
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'pgdb',
+        'PORT': 5432,
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
