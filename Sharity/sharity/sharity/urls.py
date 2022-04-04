93de538f385569ce5fea6356e5c2074d84d32575
Gökhan Üstüner <gokhanustuner@gmail.com>
2022-04-04 08:58:53 +0300

    Bug fixed that occurred during conflict resolution

--- Sharity/sharity/sharity/urls.py
+++ Sharity/sharity/sharity/urls.py
@@ -19,6 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('base.urls')),
-    path('member/', include('member.urls')),
     path('tags/', include('tags.urls'))
 ]
