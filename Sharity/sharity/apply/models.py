79833eb40545678e74663395c4ad1fb03e319fa3
Osman Selçuk Sarıoğlu <ossarioglu@gmail.com>
2022-04-17 10:32:04 +0300

    Apply Service is created

---
+++ Sharity/sharity/apply/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.db.models.deletion import CASCADE
+from django.contrib.auth.models import User
+from Sharity.sharity.offers.models import Offer
+from offers import models
+
+# unique identifiers for objects are created with this 
+import uuid
+
+
+# This object is to keep information for applications to services
+class Application(models.Model):
+
+    # requestID is created as unique id from UUID object
+    requestID = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False)
+    # serviceID has a One-to-Many relation with Service object: a service is one service from many Services
+    serviceID = models.ForeignKey(Offer, on_delete=models.CASCADE)
+    # requesterID has a One-to-Many relation with User object: a service is requested by one user from many Users
+    requesterID = models.ForeignKey(User, on_delete=models.CASCADE)
+    serviceType = models.CharField(max_length=100)
+    status = models.CharField(max_length=100)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.requestID}'
