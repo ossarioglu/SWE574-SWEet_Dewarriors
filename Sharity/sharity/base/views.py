be3d5c0743060a2dd7d1995534be90ff3992698e
Gökhan Üstüner <gokhanustuner@gmail.com>
2022-05-29 22:47:59 +0300

    Fix homepage recommendations for unauthenticated users

--- Sharity/sharity/base/views.py
+++ Sharity/sharity/base/views.py
@@ -6,25 +6,25 @@ from actstream import feeds
 from actstream.models import model_stream
 from sharity.services import SharityOfferRecommender
 import json
+import random
 
 def home(request):
     offers = Offer.objects.all()
-    result = SharityOfferRecommender.recommend(offers, request.user, 3)
 
     # Number of unread notification is send to main page
     if request.user.is_authenticated:
+        result = SharityOfferRecommender.recommend(offers, request.user, 3)
         unreadNote = request.user.receiverID.filter(status='Unread').count
         stream = model_stream(request.user)
         for i in stream:
             print(vars(i))
-        if len(result) >= 3:
-            return render(request, 'home_with_activity.html', {'offers': result, 'notes': unreadNote,"stream":stream})
+
         return render(request, 'home_with_activity.html', {'offers': result, 'notes': unreadNote,"stream":stream})
     else:
+        result = list(Offer.objects.all())
         unreadNote = ""
-        if len(result) >= 3:
-            return render(request, 'home.html', {'offers': result, 'notes': unreadNote})
-        return render(request, 'home.html', {'offers': result, 'notes': unreadNote})
+
+        return render(request, 'home.html', {'offers': random.sample(result, 3), 'notes': unreadNote})
 
 
 def notificationcount(request):
