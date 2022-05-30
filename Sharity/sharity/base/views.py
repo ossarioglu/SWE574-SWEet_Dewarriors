a91d47517d63528311f432112c6dfbd5fc904e50
Gökhan Üstüner <gokhanustuner@gmail.com>
2022-05-30 12:33:56 +0300

    Offer recommendations filtering by amendment deadline

--- Sharity/sharity/base/views.py
+++ Sharity/sharity/base/views.py
@@ -7,12 +7,14 @@ from actstream.models import model_stream
 from sharity.services import SharityOfferRecommender
 import json
 import random
+import datetime
+
 
 def home(request):
-    offers = Offer.objects.all()
 
     # Number of unread notification is send to main page
     if request.user.is_authenticated:
+        offers = Offer.objects.filter(amendment_deadline__lte=datetime.datetime.now() + datetime.timedelta(days=1))
         result = SharityOfferRecommender.recommend(offers, request.user, 3)
         unreadNote = request.user.receiverID.filter(status='Unread').count
         stream = model_stream(request.user)
